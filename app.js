d13f58d32285dad141cdd2799d2de0e91ddecb1a
sangjy15 <sangjy22@gmail.com>
2023-11-21 21:49:29 +0900

    jquery적용

--- app.js
+++ app.js
@@ -10,42 +10,41 @@ function calendar(new_year, new_month){
         day = d.getDay();
 
     // caption 영역 날짜 표시 객체
-    var caption_year = document.querySelector('.year'),
-        caption_month = document.querySelector('.month');
+    var $caption_year = $('.year'),
+        $caption_month = $('.month');
 
-    var start_day = document.querySelectorAll('tr td');
+    var $start_day = $('tr td');
 
     // 테이블 초기화
-    for(var i = 0; i < start_day.length; i++){
-        start_day[i].innerHTML = '&nbsp;';
-    }
+    $start_day.each(function() {
+        $(this).html('&nbsp;');
+    });
 
     // 한달치 날짜를 테이블에 시작 요일부터 순서대로 표시
     for(var i = day; i < day + d_length; i++){
-      start_day[i].innerHTML = date;
+      $start_day.eq(i).html(date);
       date++;
     }
 
     // caption 날짜 표시
-    caption_year.innerHTML = year;
-    caption_month.innerHTML = month + 1;		
+    $caption_year.html(year);
+    $caption_month.html(month + 1);		
 }
 
 
 (function(){
-    var prev = document.getElementById('prev'),
-        next = document.getElementById('next'),
+    var $prev = $('#prev'),
+        $next = $('#next'),
         year = new Date().getFullYear(),
         month = new Date().getMonth() + 1;
 
     calendar(year, month);
-    prev.onclick = function(){
+    $prev.click (function(){
         calendar(year, --month);
-        console.log(month);
-    };
-    next.onclick = function(){
+    });
+    $next.click(function(){
         calendar(year, ++month);
         console.log(month);
-    };		
+    });		
 
 })();
